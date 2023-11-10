659031a17399fbafd7e819e56ddd2331912815cd
Aswinkumar T S <aswinkumar.ts@experionglobal.local>
2023-11-10 11:45:22 +0530

    fix:added redirect

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -6,64 +6,6 @@ const scope = "user-read-private user-read-email"; // Add the scopes you need
 const authorizationUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
 window.location.href = authorizationUrl;
 
-// function getAuthorizationCode() {
-//   const urlParams = new URLSearchParams(window.location.search);
-//   return urlParams.get("code");
-// }
-
-// // Function to exchange authorization code for access and refresh tokens
-// async function exchangeCodeForTokens(authorizationCode) {
-//   const tokenUrl = "https://accounts.spotify.com/api/token";
-//   const data = new URLSearchParams();
-//   data.append("grant_type", "authorization_code");
-//   data.append("code", authorizationCode);
-//   data.append("redirect_uri", redirectUri);
-
-//   const authBase64 = btoa(`${clientId}:${clientSecret}`);
-
-//   return fetch(tokenUrl, {
-//     method: "POST",
-//     body: data,
-//     headers: {
-//       Authorization: `Basic ${authBase64}`,
-//       "Content-Type": "application/x-www-form-urlencoded",
-//     },
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const accessToken = data.access_token;
-//       const refreshToken = data.refresh_token;
-//       localStorage.setItem("access_token", accessToken);
-//       localStorage.setItem("refresh_token", refreshToken);
-//       // You can now use the access and refresh tokens as needed.
-//       return { accessToken, refreshToken };
-//     })
-//     .catch((error) => {
-//       console.error("Error exchanging code for tokens:", error);
-//     });
-// }
-
-// // Entry point of your application
-// async function main() {
-//   const authorizationCode = getAuthorizationCode();
-
-//   if (authorizationCode) {
-//     // You have the authorization code, proceed to exchange it for tokens
-//     await exchangeCodeForTokens(authorizationCode).then((tokens) => {
-//       // Use the access and refresh tokens as needed
-//       console.log("Access Token:", tokens.accessToken);
-//       console.log("Refresh Token:", tokens.refreshToken);
-//       getProfileData(tokens.accessToken);
-//     });
-//   } else {
-//     // The user may have denied access or there was an error.
-//     console.error("Authorization code not found.");
-//   }
-// }
-
-// // Call the main function to start the process
-// main();
-//
 function getAuthorizationCode() {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get("code");
