849afc44e6bc4d41e7837bbed89903ddc62d1b06
Hema K Shaji <hema.shaji@experionglobal.local>
2023-11-11 03:20:50 +0530

    fix : styled and aligned elements in a proper manner

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,49 +1,49 @@
-import { getData, refreshAccessToken } from "./get.js";
+import { getData, refreshAccessToken } from './get.js';
 
-const clientId = "3123b1eded6c47ab91bf1fd765a537b6";
-const clientSecret = "98598afa94de4a93b71b39e1efd13a80";
-const newRelease = "https://api.spotify.com/v1/browse/new-releases";
-const featured = "https://api.spotify.com/v1/browse/featured-playlists";
+const clientId = '3123b1eded6c47ab91bf1fd765a537b6';
+const clientSecret = '98598afa94de4a93b71b39e1efd13a80';
+const newRelease = 'https://api.spotify.com/v1/browse/new-releases';
+const featured = 'https://api.spotify.com/v1/browse/featured-playlists';
 const romantic =
-  "https://api.spotify.com/v1/browse/categories/romance/playlists";
+  'https://api.spotify.com/v1/browse/categories/romance/playlists';
 const albums =
-  "https://api.spotify.com/v1/albums?ids=382ObEPsp2rxGrnsizN5TX%2C1A2GTWGtFfWp7KSQTwWOyo%2C2noRn2Aes5aoNVsU6iWThc&market=IN";
+  'https://api.spotify.com/v1/albums?ids=382ObEPsp2rxGrnsizN5TX%2C1A2GTWGtFfWp7KSQTwWOyo%2C2noRn2Aes5aoNVsU6iWThc&market=IN';
 const shows =
-  "https://api.spotify.com/v1/shows?ids=5CfCWKI5pZ28U0uOzXkDHe%2C5as3aKmN2k11yfDDDSrvaZ";
+  'https://api.spotify.com/v1/shows?ids=5CfCWKI5pZ28U0uOzXkDHe%2C5as3aKmN2k11yfDDDSrvaZ';
 
 const indiantop =
-  "https://api.spotify.com/v1/artists/4YRxDV8wJFPHPTeXepOstw/top-tracks?market=IN";
+  'https://api.spotify.com/v1/artists/4YRxDV8wJFPHPTeXepOstw/top-tracks?market=IN';
 
 const anirudh =
-  "https://api.spotify.com/v1/artists/4zCH9qm4R2DADamUHMCa6O/albums";
+  'https://api.spotify.com/v1/artists/4zCH9qm4R2DADamUHMCa6O/albums';
 
-await refreshAccessToken(localStorage.getItem("refresh_token"));
+await refreshAccessToken(localStorage.getItem('refresh_token'));
 
 const newReleasesPromise = getData(newRelease);
 
-newReleasesPromise.then((data) => {
+newReleasesPromise.then(data => {
   console.log(data);
   const newAlbums = [...data.albums.items];
-  newAlbums.forEach((albums) => {
-    let newReleaseContainer = document.querySelector(".newReleases");
-    let itemTile = document.createElement("div");
-    itemTile.classList.add("itemTile");
+  newAlbums.forEach(albums => {
+    let newReleaseContainer = document.querySelector('.newReleases');
+    let itemTile = document.createElement('div');
+    itemTile.classList.add('itemTile');
 
-    let albumImage = document.createElement("img");
-    albumImage.classList.add("albumImage");
+    let albumImage = document.createElement('img');
+    albumImage.classList.add('albumImage');
     albumImage.src = albums.images[0].url; // Assuming you want to use the first image
-    let albumTitle = document.createElement("h4");
-    albumTitle.classList.add("songTitle");
+    let albumTitle = document.createElement('h4');
+    albumTitle.classList.add('songTitle');
     albumTitle.textContent = albums.name;
 
-    let artistTitle = document.createElement("p");
-    artistTitle.classList.add("albumTitle");
+    let artistTitle = document.createElement('p');
+    artistTitle.classList.add('albumTitle');
     // Assuming you want to display the first artist's name
     artistTitle.textContent = albums.artists[0].name;
 
     itemTile.append(albumImage, albumTitle, artistTitle);
     itemTile.onclick = () => {
-      loadPage("musiclist", albums.id);
+      loadPage('musiclist', albums.id);
     };
     newReleaseContainer.append(itemTile);
   });
@@ -51,24 +51,24 @@ newReleasesPromise.then((data) => {
 
 const romanticPromise = getData(romantic);
 
-romanticPromise.then((data) => {
+romanticPromise.then(data => {
   console.log(data);
 
   const newRomantic = [...data.playlists.items];
-  newRomantic.forEach((playlists) => {
+  newRomantic.forEach(playlists => {
     // console.log(playlists);
-    let newReleaseContainer = document.querySelector(".romantic");
-    let itemTile = document.createElement("div");
-    itemTile.classList.add("itemTile");
+    let newReleaseContainer = document.querySelector('.romantic');
+    let itemTile = document.createElement('div');
+    itemTile.classList.add('itemTile');
 
-    let albumImage = document.createElement("img");
-    albumImage.classList.add("albumImage");
+    let albumImage = document.createElement('img');
+    albumImage.classList.add('albumImage');
     albumImage.src = playlists.images[0].url; // Assuming you want to use the first image
-    let albumTitle = document.createElement("h4");
-    albumTitle.classList.add("songTitle");
+    let albumTitle = document.createElement('h4');
+    albumTitle.classList.add('songTitle');
     albumTitle.textContent = playlists.name;
-    let artistTitle = document.createElement("p");
-    artistTitle.classList.add("albumTitle");
+    let artistTitle = document.createElement('p');
+    artistTitle.classList.add('albumTitle');
     // Assuming you want to display the first artist's name
     artistTitle.textContent = playlists.description;
 
@@ -78,21 +78,21 @@ romanticPromise.then((data) => {
 });
 
 const episodes = getData(indiantop);
-episodes.then((data) => {
+episodes.then(data => {
   console.log(data);
 
   const topSongs = [...data.tracks];
-  topSongs.forEach((playlists) => {
+  topSongs.forEach(playlists => {
     // console.log(playlists);
-    let top10Container = document.querySelector(".topTen");
-    let itemTile = document.createElement("div");
-    itemTile.classList.add("itemTile");
+    let top10Container = document.querySelector('.topTen');
+    let itemTile = document.createElement('div');
+    itemTile.classList.add('itemTile');
 
-    let albumImage = document.createElement("img");
-    albumImage.classList.add("albumImage");
+    let albumImage = document.createElement('img');
+    albumImage.classList.add('albumImage');
     albumImage.src = playlists.album.images[0].url; // Assuming you want to use the first image
-    let albumTitle = document.createElement("h4");
-    albumTitle.classList.add("songTitle");
+    let albumTitle = document.createElement('h4');
+    albumTitle.classList.add('songTitle');
     albumTitle.textContent = playlists.album.name;
     itemTile.append(albumImage, albumTitle);
     itemTile.onclick = () => {
@@ -107,16 +107,16 @@ const anirudhAlbums = getData(anirudh);
 anirudhAlbums.then((data, index) => {
   // console.log(data);
   const anirudhAlbums = [...data.items];
-  anirudhAlbums.forEach((playlists) => {
+  anirudhAlbums.forEach(playlists => {
     // console.log(playlists);
-    let anirudhContainer = document.querySelector(".anirudhMania");
-    let itemTile = document.createElement("div");
-    itemTile.classList.add("itemTile");
-    let albumImage = document.createElement("img");
-    albumImage.classList.add("albumImage");
+    let anirudhContainer = document.querySelector('.anirudhMania');
+    let itemTile = document.createElement('div');
+    itemTile.classList.add('itemTile');
+    let albumImage = document.createElement('img');
+    albumImage.classList.add('albumImage');
     albumImage.src = playlists.images[0].url;
-    let albumTitle = document.createElement("h4");
-    albumTitle.classList.add("songTitle");
+    let albumTitle = document.createElement('h4');
+    albumTitle.classList.add('songTitle');
     albumTitle.textContent = playlists.name;
     itemTile.append(albumImage, albumTitle);
     anirudhContainer.append(itemTile);
---
+++ assets/js/specificGenre.js
@@ -0,0 +1,101 @@
+import { getData } from './get.js';
+
+// Replace with the specific Spotify API endpoint you want to access
+const apiEndpoint = 'https://api.spotify.com/v1/browse/new-releases';
+// const apiEndpoint = "https://api.spotify.com/v1/browse/featured-playlists";
+
+// const apiEndpoint =
+//   "https://api.spotify.com/v1/browse/categories/romance/playlists";
+
+const clientId = '3123b1eded6c47ab91bf1fd765a537b6';
+const clientSecret = '98598afa94de4a93b71b39e1efd13a80';
+const redirectUri = 'http://127.0.0.1:5500/home.html';
+const scope = 'user-read-private user-read-email';
+
+async function refreshAccessToken(refreshToken) {
+  const tokenUrl = 'https://accounts.spotify.com/api/token';
+  const data = new URLSearchParams();
+  data.append('grant_type', 'refresh_token');
+  data.append('refresh_token', refreshToken);
+  const authBase64 = btoa(`${clientId}:${clientSecret}`);
+  const response = await fetch(tokenUrl, {
+    method: 'POST',
+    body: data,
+    headers: {
+      Authorization: `Basic ${authBase64}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+  if (response.ok) {
+    const tokenData = await response.json();
+    console.log(tokenData.access_token);
+    localStorage.setItem('access_token', tokenData.access_token);
+    localStorage.setItem(
+      'refresh_token',
+      tokenData.refresh_token || refreshToken
+    );
+    return {
+      accessToken: tokenData.access_token,
+      // Note: The refresh token might be the same or a new one
+      refreshToken: tokenData.refresh_token || refreshToken,
+    };
+  } else {
+    throw new Error('Failed to refresh access token');
+  }
+}
+
+await refreshAccessToken(localStorage.getItem('refresh_token'));
+
+const specificGenre = getData(apiEndpoint);
+
+// const gradientBox = document.getElementById('section-spotify-playlists');
+// gradientBox.style.background =
+//   'linear-gradient(to bottom, #ff00ff 30%, #000000 40%)';
+let specificGenreContainer = document.querySelector('.spotify-playlists');
+const listDiv = document.createElement('div');
+listDiv.classList.add('list');
+specificGenre.then(data => {
+  console.log(data);
+  const genre = [...data.albums.items];
+  genreTitle.classList.add('genreTitle');
+  genreTitle.textContent = genre[0].album_type;
+
+  genre.forEach(tracks => {
+    const dynamicDiv = document.createElement('div');
+    dynamicDiv.classList.add('item');
+    const genreTitle = document.getElementById('genreTitle');
+
+    let imgElement = document.createElement('img');
+    imgElement.src = tracks.images[0].url; // Assuming you want to use the first image
+
+    const playDiv = document.createElement('div');
+    playDiv.classList.add('play');
+    const playSpan = document.createElement('span');
+    playSpan.classList.add('fa', 'fa-play');
+
+    let trackTitle = document.createElement('h4');
+    trackTitle.classList.add('songTitle');
+    trackTitle.textContent = tracks.name;
+
+    let tarckInfo = document.createElement('p');
+    tarckInfo.textContent = tracks.artists[0].name;
+
+    playDiv.appendChild(playSpan);
+    dynamicDiv.append(imgElement, playDiv, trackTitle, tarckInfo);
+    listDiv.appendChild(dynamicDiv);
+    specificGenreContainer.append(listDiv);
+  });
+});
+
+const navigate = destination => {
+  console.log('click');
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      document.querySelector('.homeWrapper').innerHTML = xhr.responseText;
+    }
+  };
+  xhr.open('GET', `http://127.0.0.1:5500/${destination}.html`, true);
+  xhr.send();
+};
