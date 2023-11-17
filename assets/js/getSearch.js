27fe3fa9147fa38ee0eb78f7ae645a163105eb12
Hema K Shaji <hema.shaji@experionglobal.local>
2023-11-17 18:55:33 +0530

    feat:added specific page

--- assets/js/getSearch.js
+++ assets/js/getSearch.js
@@ -1,22 +1,22 @@
-import { getData, refreshAccessToken } from "./get.js";
+import { getData, refreshAccessToken } from './get.js';
 
-await refreshAccessToken(localStorage.getItem("refresh_token"));
+await refreshAccessToken(localStorage.getItem('refresh_token'));
 const searchCateogoryPromise = getData(
-  "https://api.spotify.com/v1/browse/categories?country=IN"
+  'https://api.spotify.com/v1/browse/categories?country=IN'
 );
 
-searchCateogoryPromise.then((data) => {
+searchCateogoryPromise.then(data => {
   const categoryData = [...data.categories.items];
   console.log(categoryData);
-  const searchSection = document.querySelector("section.searchDisplay");
-  categoryData.forEach((elements) => {
-    const searchContainer = document.createElement("div");
-    searchContainer.classList.add("search-block");
+  const searchSection = document.querySelector('section.searchDisplay');
+  categoryData.forEach(elements => {
+    const searchContainer = document.createElement('div');
+    searchContainer.classList.add('search-block');
 
-    const title = document.createElement("h2");
+    const title = document.createElement('h2');
     title.textContent = elements.name;
 
-    let imageElement = document.createElement("img");
+    let imageElement = document.createElement('img');
     imageElement.src = elements.icons[0].url;
 
     searchContainer.append(title, imageElement);
@@ -38,9 +38,9 @@ function getRandomColor() {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
-const searchBar = document.querySelector(".search-input");
-searchBar.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
+const searchBar = document.querySelector('.search-input');
+searchBar.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
     window.location.href = `searchList.html?q=${searchBar.value}`;
   }
 });
