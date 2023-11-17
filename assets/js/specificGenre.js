0b0f52a9f883c27e4e962db151f17ec505b10975
Hema K Shaji <hema.shaji@experionglobal.local>
2023-11-17 18:55:33 +0530

    feat:added specific page

--- assets/js/specificGenre.js
+++ assets/js/specificGenre.js
@@ -1,99 +1,55 @@
 import { getData } from './get.js';
-
-// Replace with the specific Spotify API endpoint you want to access
-const apiEndpoint = 'https://api.spotify.com/v1/browse/new-releases';
-// const apiEndpoint = "https://api.spotify.com/v1/browse/featured-playlists";
-
-// const apiEndpoint =
-//   "https://api.spotify.com/v1/browse/categories/romance/playlists";
-
-const clientId = '3123b1eded6c47ab91bf1fd765a537b6';
-const clientSecret = '98598afa94de4a93b71b39e1efd13a80';
-const redirectUri = 'http://127.0.0.1:5500/home.html';
-const scope = 'user-read-private user-read-email';
-
-async function refreshAccessToken(refreshToken) {
-  const tokenUrl = 'https://accounts.spotify.com/api/token';
-  const data = new URLSearchParams();
-  data.append('grant_type', 'refresh_token');
-  data.append('refresh_token', refreshToken);
-  const authBase64 = btoa(`${clientId}:${clientSecret}`);
-  const response = await fetch(tokenUrl, {
-    method: 'POST',
-    body: data,
-    headers: {
-      Authorization: `Basic ${authBase64}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-  if (response.ok) {
-    const tokenData = await response.json();
-    console.log(tokenData.access_token);
-    localStorage.setItem('access_token', tokenData.access_token);
-    localStorage.setItem(
-      'refresh_token',
-      tokenData.refresh_token || refreshToken
-    );
-    return {
-      accessToken: tokenData.access_token,
-      // Note: The refresh token might be the same or a new one
-      refreshToken: tokenData.refresh_token || refreshToken,
-    };
-  } else {
-    throw new Error('Failed to refresh access token');
-  }
-}
+import { refreshAccessToken } from './main.js';
 
 await refreshAccessToken(localStorage.getItem('refresh_token'));
 
-const specificGenre = getData(apiEndpoint);
+const id = new URLSearchParams(window.location.search).get('id');
+const url = `https://api.spotify.com/v1/browse/categories/${id}`;
 
-//to display
-let specificGenreContainer = document.querySelector('.spotify-playlists');
-const listDiv = document.createElement('div');
-// document.getElementById('genreTitle').textContent = 'Tamil';
-listDiv.classList.add('list');
-specificGenre.then(data => {
-  console.log(data);
-  const genre = [...data.albums.items];
-  //   genreTitle.classList.add('genreTitle');
-  //   genreTitle.textContent = genre[0].album_type;
+console.log(url);
+const genreData = await getData(url);
+const playlistData = await getData(url + '/playlists');
 
-  genre.forEach(tracks => {
-    const dynamicDiv = document.createElement('div');
-    dynamicDiv.classList.add('item');
+const genreTitle = document.querySelector('#genre-title');
+genreTitle.innerHTML = genreData.name;
 
-    let imgElement = document.createElement('img');
-    imgElement.src = tracks.images[0].url; // Assuming you want to use the first image
+const mainContainer = document.getElementById('main-container');
+const container = document.createElement('div');
+container.className = 'playlist-container';
+// const playlistsData = [...playlistData.playlists.items];
+// console.log(playlistsData);
 
-    // const playDiv = document.createElement('div');
-    // playDiv.classList.add('play');
-    // const playSpan = document.createElement('span');
-    // playSpan.classList.add('fa', 'fa-play');
+playlistData.playlists.items.forEach(playlist => {
+  console.log(playlist);
+  const playlistLink = document.createElement('a');
+  const playlistUrl = `musiclist.html?id=${playlist.id}`;
 
-    let trackTitle = document.createElement('h4');
-    trackTitle.classList.add('songTitle');
-    trackTitle.textContent = tracks.name;
+  playlistLink.setAttribute('href', playlistUrl);
 
-    let tarckInfo = document.createElement('p');
-    tarckInfo.textContent = tracks.artists[0].name;
+  const playlist_card = document.createElement('div');
+  playlist_card.className = 'playlist-card';
 
-    // playDiv.appendChild(playSpan);
-    dynamicDiv.append(imgElement, trackTitle, tarckInfo);
-    listDiv.appendChild(dynamicDiv);
-    specificGenreContainer.append(listDiv);
-  });
+  const interdata = getData(playlist.href);
+  console.log(interdata);
+
+  const image_card_maindiv = document.createElement('div');
+  const image_div = document.createElement('div');
+  image_div.className = 'image_div';
+
+  const img_element = document.createElement('img');
+  img_element.classList.add('img_element');
+  img_element.src = playlist.images[0].url;
+
+  const playlist_title = document.createElement('h3');
+  playlist_title.className = 'playlist_title';
+  playlist_title.innerHTML = playlist.name;
+
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.innerHTML = playlist.description;
+
+  image_card_maindiv.append(image_div, img_element, description);
+  playlist_card.append(img_element, playlist_title, description);
+  playlistLink.appendChild(playlist_card);
+  mainContainer.appendChild(playlistLink);
 });
-
-const navigate = destination => {
-  console.log('click');
-
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      document.querySelector('.homeWrapper').innerHTML = xhr.responseText;
-    }
-  };
-  xhr.open('GET', `http://127.0.0.1:5500/${destination}.html`, true);
-  xhr.send();
-};
