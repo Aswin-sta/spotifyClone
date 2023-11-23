d31127c9ac50adf85e89ab1c0d7381f7c5ddce9a
Abhijit Thankachan <abhijit.thankachan@experionglobal.local>
2023-11-23 12:28:22 +0530

    fix:Optimising code

--- assets/js/getSearch.js
+++ assets/js/getSearch.js
@@ -1,47 +1,67 @@
 import { getData, refreshAccessToken } from './get.js';
-
-await refreshAccessToken(localStorage.getItem('refresh_token'));
-const searchCateogoryPromise = getData(
-  'https://api.spotify.com/v1/browse/categories?country=IN'
-);
-
-searchCateogoryPromise.then(data => {
-  const categoryData = [...data.categories.items];
-  console.log(categoryData);
-  const searchSection = document.querySelector('section.searchDisplay');
-  categoryData.forEach(elements => {
-    const searchContainer = document.createElement('div');
-    searchContainer.classList.add('searchBlock');
-
-    const title = document.createElement('h2');
-    title.textContent = elements.name;
-
-    let imageElement = document.createElement('img');
-    imageElement.src = elements.icons[0].url;
-
-    searchContainer.append(title, imageElement);
-    searchSection.appendChild(searchContainer);
-
-    searchContainer.onclick = () => {
-      window.location.href = `specificGenre.html?id=${elements.id}`;
-      // window.location.href = `specificGenre.html?url=${elements.href}`;
-    };
-
-    const randomColor = getRandomColor();
-    searchContainer.style.backgroundColor = randomColor;
-  });
-});
-//Function to generate random colours
-function getRandomColor() {
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
-  return `rgb(${red}, ${green}, ${blue})`;
+// Function to fetch data 
+async function fetchData() {
+  await refreshAccessToken(localStorage.getItem('refresh_token'));
+  const data = await getData('https://api.spotify.com/v1/browse/categories?country=IN');
+  return data.categories.items.map((element) => ({
+    name: element.name,
+    imageUrl: element.icons[0].url,
+    id: element.id,
+  }));
 }
 
+// Function to generate random colours
+const getRandomColor = () => `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+
+// Function to create search container
+function createSearchContainer(element) {
+  const searchContainer = document.createElement('div');
+  searchContainer.classList.add('searchBlock');
+
+  const title = document.createElement('h2');
+  title.textContent = element.name;
+
+  const imageElement = document.createElement('img');
+  imageElement.src = element.imageUrl;
+  imageElement.alt = 'Category Cover';
+
+  searchContainer.append(title, imageElement);
+
+  searchContainer.onclick = () => {
+    window.location.href = `specificGenre.html?id=${element.id}`;
+  };
+
+  const randomColor = getRandomColor();
+  searchContainer.style.backgroundColor = randomColor;
+
+  return searchContainer;
+}
+
+// Entry point 
+async function main() {
+  try {
+    const categoryData = await fetchData();
+
+    console.log(categoryData);
+
+    const searchSection = document.querySelector('section.searchDisplay');
+
+    categoryData.forEach((element) => {
+      const searchContainer = createSearchContainer(element);
+      searchSection.appendChild(searchContainer);
+    });
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+// Event listener for search input
 const searchBar = document.querySelector('.searchInput');
-searchBar.addEventListener('keydown', event => {
+searchBar.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     window.location.href = `searchList.html?q=${searchBar.value}`;
   }
 });
+
+
+main();
