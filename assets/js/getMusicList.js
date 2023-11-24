1c2e276f7b670d940f62db70925bbdec1604cef6
Libin Prince <libin.prince@experionglobal.local>
2023-11-24 18:28:12 +0530

    fix:bootstrap

--- assets/js/getMusicList.js
+++ assets/js/getMusicList.js
@@ -23,39 +23,46 @@ function timeConvertion(duration_ms) {
 }
 
 newReleasesPromise.then((data) => {
+  console.log("hi");
+  console.log(data);
   //bannerImage
   const bannerImage = document.createElement("img");
-  bannerImage.className = "bannerImgImg skeleton";
-  const bannerImageSection = document.querySelector(".bannerImg");
+  bannerImage.className = "bannerImgImg";
+  const bannerImageSection = document.getElementById("playListHeaderBanner");
   bannerImageSection.append(bannerImage);
   bannerImage.src = data.images[0].url;
 
   //banner Title
-  const bannerTitle = document.querySelector(".bannerHeader h1");
+  const bannerTitle = document.createElement("h1");
   bannerTitle.textContent = data.name;
+  const htmlBannerTitle = document.getElementById("playListHeaderTitle");
+  htmlBannerTitle.append(bannerTitle);
 
   //banner description
-  const bannerDescription = document.querySelector(".bannerHeaderDesc");
+  const bannerDescription = document.createElement("h6");
   bannerDescription.textContent = data.description;
+  const bannerDescriptionHtml = document.getElementById("playListHeaderDesc");
+  bannerDescriptionHtml.append(bannerDescription);
 
   const musicDataList = data.tracks.items;
-  const albumSongList = document.querySelector(".albumSongList");
+  const albumSongList = document.getElementById("playListTrackLists");
 
   musicDataList.forEach((track, index) => {
     const songListTrack = document.createElement("div");
-    songListTrack.className = "songListTrack";
+    songListTrack.className = "songListTrack row";
     songListTrack.id = `Track-${Number(index + 1)}`;
 
     const trackNo = document.createElement("p");
-    trackNo.className = "titleHash";
+    trackNo.className = "titleHash col-1 ";
     trackNo.textContent = `${Number(index + 1)}`;
 
     const trackName = document.createElement("p");
-    trackName.className = "titleTitleName";
+    trackName.className =
+      "titleTitleName col-lg-10 col-md-10 col-xl-10 col-xxl-10 col-sm-10 col-8";
     trackName.textContent = type === "playlist" ? track.track.name : track.name;
 
     const trackDuration = document.createElement("p");
-    trackDuration.className = "titleSpotifyDuration";
+    trackDuration.className = "titleSpotifyDuration col-1";
     trackDuration.textContent = timeConvertion(
       type === "playlist" ? track.track.duration_ms : track.duration_ms
     );
